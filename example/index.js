acb95bab11affd81f345afe9a0d96e7380aa2000
Quenteon <quanteonquantumelysium@gmail.com>
2024-12-06 01:16:12 +0000

    success

--- example/index.js
+++ example/index.js
@@ -7,7 +7,7 @@ const workflowDefinition = {
             "type": "action",
             "task": "collectData",
             "parameters": { "url": "http://example.com", "method": "GET" },
-            "onSuccess": "step5",
+            "onSuccess": "step2",
             "onFailure": "step5",
         },
         {
@@ -45,10 +45,42 @@ const workflowDefinition = {
     ]
 };
 const workflowDefinitionNew = {
-    "name": "dataCollection",
+    "name": "example",
     "steps": [
         {
             "name": "step1",
+            "type": "action",
+            "task": "collectData",
+            "parameters": { "url": "http://example.com", "method": "GET" },
+            "onSuccess": "step2",
+            "onFailure": "step5",
+        },
+        {
+            "name": "step2",
+            "type": "delay",
+            "task": "waitData",
+            "parameters": { "duration": 5000 },
+            "onSuccess": "step3",
+            "onFailure": "step5",
+        },
+        {
+            "name": "step3",
+            "type": "action",
+            "task": "validateData",
+            "onSuccess": "step4",
+            "onFailure": "step5",
+            "parameters": {}
+        },
+        {
+            "name": "step4",
+            "type": "action",
+            "task": "finalizeData",
+            "onSuccess": null,
+            "onFailure": "step5",
+            "parameters": {}
+        },
+        {
+            "name": "step5",
             "type": "action",
             "task": "handleFailure",
             "onSuccess": null,
